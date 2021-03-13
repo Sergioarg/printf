cfe3474b29da384334c6a185a555f0aa590a8242
jeanpierreba <2713@holbertonschool.com>
2021-03-13 23:40:14 +0000

    updated header with functions

--- holberton.h
+++ holberton.h
@@ -3,7 +3,19 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdarg.h>
+
+typedef struct handleSpecifier
+{
+	char c;
+	int (*f)(va_list percent);
+} con_spe;
 
 int _printf(const char *format, ...);
+void _putchar(char character);
+int sing_char(char c);
+char *str_concat(char *s1, char *s2);
 
 #endif
