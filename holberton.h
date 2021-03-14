6021a831f6771839ef997b21a33ffd12772db2c1
Manu Bedoya <manuelfernandobedoya@gmail.com>
2021-03-14 15:19:10 -0500

    Update the header and _printf files with change the function with structures

--- holberton.h
+++ holberton.h
@@ -7,9 +7,19 @@
 #include <string.h>
 #include <stdarg.h>
 
+typedef struct handleSpecifier
+{
+	char c;
+	int (*f)(va_list arguments);
+} con_spe;
+
+
 int _printf(const char *format, ...);
-void _putchar(char character);
-int sing_char(char c);
+int _putchar(char character);
+int sing_char(va_list selector);
+int (*conversion_specifiers(char c))();
 char *str_concat(char *s1, char *s2);
+int str_char(va_list selector);
+int percent_sign(va_list selector);
 
 #endif
