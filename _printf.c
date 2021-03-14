ce6d144260be23732474b9199f4cf902ded76ea5
Manu Bedoya <manuelfernandobedoya@gmail.com>
2021-03-13 19:07:15 -0500

    Update the file _printf where already print %c %s and %%

--- _printf.c
+++ _printf.c
@@ -1,20 +1,48 @@
 #include "holberton.h"
 
 /**
- * _printf - print
- * @format: String
- * Return: 0 or -1
- */
+ * _printf - Print Anything (integers, strings, floats, doubles , etc)
+ * @format: The part of string
+ * Also recive a variadic arguments
+ * Return: 0 always
+*/
 int _printf(const char *format, ...)
 {
-	int i, len;
+	int i, j, len;
+	char *tmp;
+	va_list arguments;
 
 	if (!format)
-	return (-1);
-
-	for (i = 0; format[i] != '\0'; i++)
-
-	len = i + 1;
-	write(1, format, len);
+		return (0);
+	va_start(arguments, format);
+	for (i = 0; format[i]; i++)
+	{
+		if (format[i] == '%')
+		{
+			if (format[i + 1] == '%')
+			{
+				_putchar('%');
+				_putchar('\n');
+				i++;
+			}
+			else if (format[i + 1] == 'c')
+			{
+				_putchar(va_arg(arguments, int));
+				_putchar('\n');
+				i++;
+			}
+			else if (format[i + 1] == 's')
+			{
+				tmp = va_arg(arguments, char *);
+				for (j = 0; tmp[j]; j++)
+					_putchar(tmp[j]);
+				i++;
+			}
+			else
+				_putchar(format[i]);
+		}
+		else
+			_putchar(format[i]);
+	}
 	return (0);
 }
