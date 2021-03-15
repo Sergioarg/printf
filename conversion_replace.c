8d248c92958e7ab5539642298755a5e2e020a80c
jeanpierreba <2713@holbertonschool.com>
2021-03-15 15:43:19 +0000

    added description to functions

--- conversion_replace.c
+++ conversion_replace.c
@@ -1,29 +1,55 @@
 #include "holberton.h"
 
-int sing_char(va_list selector)
+/**
+ * sing_char - print single character
+ * @selector: condition that you are going to print
+ * @p: is the length of the string
+ * Return: Always 0.
+ */
+
+int sing_char(va_list selector, int *p)
 {
 	_putchar(va_arg(selector, int));
-	return 0;
+	*p = *p + 1;
+	return (0);
 }
 
-int str_char(va_list selector)
+/**
+ * str_char - print a string of characters
+ * @selector: condition that you are going to print
+ * @p: is the length of the string
+ * Return: The iterator to count.
+ */
+
+int str_char (va_list selector, int *p)
 {
 	char *s = va_arg(selector, char *);
-  int i;
+	int i;
 
-	if(*s == '\0')
+	if (*s == '\0')
 		return (-1);
 
 	if (*s == '%')
 		return (-1);
 
-  for (i = 0; s[i]; i++)
-    _putchar(s[i]);
-
+	for (i = 0; s[i]; i++)
+	{
+		_putchar(s[i]);
+		*p = *p + 1;
+	}
 	return (i);
 }
 
-int percent_sign(va_list selector)
+/**
+ * percent_sign - print a single percentage
+ * @selector: condition that you are going to print
+ * @p: is the length of the string
+ * Return: Always 0.
+ */
+
+int percent_sign(va_list selector, int *p)
 {
-	return (_putchar('%'));
+	_putchar('%');
+	*p = *p + 1;
+	return (0);
 }
