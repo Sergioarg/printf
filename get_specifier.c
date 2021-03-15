6d60a6c594d2985fee57333000176a7597ebc33d
jeanpierreba <2713@holbertonschool.com>
2021-03-15 16:00:17 +0000

    added description

--- get_specifier.c
+++ get_specifier.c
@@ -1,21 +1,25 @@
 #include "holberton.h"
+/**
+	* conversion_specifiers - convert the specifer according to the case
+	* @c: is the char that specifies the case
+	* Return: NULL.
+	*/
 
 int (*conversion_specifiers(char c))()
 {
 	con_spe cs_arr[] = {
-  	{'c', sing_char},
-	{'s', str_char},
-    {'%', percent_sign},
-	{'d', print_integer},
-	{'i', print_integer},
-	{'\0', NULL}
-	};
+		{'c', sing_char},
+		{'s', str_char},
+		{'%', percent_sign},
+		{'d', print_integer},
+		{'i', print_integer},
+		{'\0', NULL}};
 	int i = 0;
 
-	while(cs_arr[i].c != '\0')
+	while (cs_arr[i].c != '\0')
 	{
 		if (cs_arr[i].c == c)
-		  return(cs_arr[i].f);
+			return (cs_arr[i].f);
 		i++;
 	}
 	return (NULL);
