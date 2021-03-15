47c2c70f7cf5bb85a8aca9927857a1130240a876
Sergioarg <sergio_ramos53@yahoo.com>
2021-03-15 11:50:10 -0500

    variable rearrangement

--- get_specifier.c
+++ get_specifier.c
@@ -1,20 +1,22 @@
 #include "holberton.h"
 /**
-	* conversion_specifiers - convert the specifer according to the case
-	* @c: is the char that specifies the case
-	* Return: NULL.
-	*/
+ * conversion_specifiers - convert the specifer according to the case
+ * @c: is the char that specifies the case
+ * Return: NULL.
+ */
 
 int (*conversion_specifiers(char c))()
 {
+	int i = 0;
+
 	con_spe cs_arr[] = {
 		{'c', sing_char},
 		{'s', str_char},
 		{'%', percent_sign},
 		{'d', print_integer},
 		{'i', print_integer},
-		{'\0', NULL}};
-	int i = 0;
+		{'\0', NULL}
+	};
 
 	while (cs_arr[i].c != '\0')
 	{
