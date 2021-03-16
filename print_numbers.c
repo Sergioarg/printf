38a4fb2049588336fb26989b2323aef9014006b9
Manu Bedoya <manuelfernandobedoya@gmail.com>
2021-03-16 00:10:30 -0500

    Add the styles in printNumbers

--- print_numbers.c
+++ print_numbers.c
@@ -7,18 +7,18 @@
  */
 int print_integerRecursive(int n)
 {
-  unsigned int i, j;
-  int count = 0;
+	unsigned int i, j;
+	int count = 0;
 
-  if (n != 0)
-  {
-    i = (n / 10);
-    j = (n % 10);
-    count += print_integerRecursive(i);
-    count++;
-    putchar_func(j + '0');
-    return (count);
-  }
+	if (n != 0)
+	{
+		i = (n / 10);
+		j = (n % 10);
+		count += print_integerRecursive(i);
+		count++;
+		putchar_func(j + '0');
+		return (count);
+	}
 	return (0);
 }
 
@@ -29,25 +29,25 @@ int print_integerRecursive(int n)
  */
 int print_integer(va_list i)
 {
-  int j;
-  int k = 0;
-  unsigned int l;
+	int j;
+	int k = 0;
+	unsigned int l;
 
-  j = va_arg(i, int);
-  if (j < 0)
-  {
-    putchar_func('-');
-    k = 1;
-    l = j * -1;
-  }
-  else
-  {
-    l = j;
-  }
-  if (l > 9)
-  {
-    return (k + print_integerRecursive(l));
-  }
-  putchar_func(l + '0');
-  return 1 + k;
+	j = va_arg(i, int);
+	if (j < 0)
+	{
+		putchar_func('-');
+		k = 1;
+		l = j * -1;
+	}
+	else
+	{
+		l = j;
+	}
+	if (l > 9)
+	{
+		return (k + print_integerRecursive(l));
+	}
+	putchar_func(l + '0');
+	return (1 + k);
 }
