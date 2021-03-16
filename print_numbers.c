5bf918f626b9c4c72bad9e7fecd280b540db8fe4
Manu Bedoya <manuelfernandobedoya@gmail.com>
2021-03-15 23:12:24 -0500

    Adding the function print_integer to print integers

--- print_numbers.c
+++ print_numbers.c
@@ -1,36 +1,11 @@
 #include "holberton.h"
-/**
- * integer_evaluator - evaluates what kind of integer is
- * @n: is the integer to evalue
- * Return: Always 0.
-  */
-int integer_evaluator(int n)
-{
-	if (n >= 0 && n <= 9)
-		return (n);
-	return (0);
-}
 
-/**
- * _abs - print the entire value of a int
- * @r: value of the integer
- * Return: entire value of r
- */
-int _abs(int r)
-{
-	if (r >= 0)
-	{
-		return (r);
-	}
-	return (-r);
-}
 /**
  * print_integerRecursive - makes a journey of the numbers
  * @n: is the number of the whole allocated
- * @p: print pointer
  * Return: 1.
  */
-int print_integerRecursive(int n, int *p)
+int print_integerRecursive(int n)
 {
 	if (n == '\0')
 	{
@@ -38,9 +13,28 @@ int print_integerRecursive(int n, int *p)
 	}
 	else
 	{
-		print_integerRecursive(n / 10, p);
-		*p = *p + 1;
+		print_integerRecursive(n / 10);
 		putchar_func((n % 10) + '0');
 	}
+	return (1);
+}
+
+/**
+ * print_integer - print a integer
+ * @i: integer to print
+ * Return: 1.
+ */
+int print_integer(va_list i)
+{
+	int number = va_arg(i, int);
+
+	if (number < 0)
+	{
+		putchar_func('-');
+		number *= -1;
+	}
+
+	print_integerRecursive(number);
+
 	return (1);
 }
